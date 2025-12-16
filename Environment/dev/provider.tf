c26b470b4801f83ae1e9ce8687699c518c31155c
Muhammad K Javed <mkj69@hotmail.com>
2025-12-16 22:47:24 +0000

    updated provider

--- Environment/dev/provider.tf
+++ Environment/dev/provider.tf
@@ -12,14 +12,15 @@ terraform {
 }
 terraform {
   backend "azurerm" {
-    resource_group_name  = "b17g30"            # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "b17g30stg"         # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "cicdcontainer"         # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "dev.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    resource_group_name  = "b17g30"           
+    storage_account_name = "b17g30stg"         
+    container_name       = "cicdcontainer"         
+    key                  = "dev.terraform.tfstate" 
+    use_oidc             = true
   }
 }
 provider "azurerm" {
   features {}
-  subscription_id = "8b0422c9-d3b4-4ad5-b676-1cd162a61f87"
+  use_oidc = true
 }
 provider "random" {}
